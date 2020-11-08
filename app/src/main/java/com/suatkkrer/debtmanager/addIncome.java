ec5e3a1afd296bc4e997453cae52de6f247eb7f5
suatkkrer <suat.kukrer@gmail.com>
2020-11-08 14:39:38 +0100

    background

---
+++ app/src/main/java/com/suatkkrer/debtmanager/addIncome.java
@@ -0,0 +1,14 @@
+package com.suatkkrer.debtmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addIncome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_income);
+    }
+}
---
+++ app/src/main/java/com/suatkkrer/debtmanager/addOutCome.java
@@ -0,0 +1,14 @@
+package com.suatkkrer.debtmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addOutCome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_out_come);
+    }
+}
